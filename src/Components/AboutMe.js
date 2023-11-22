4573f2c9512b4ee0140cdb17c54f7fe94915ba06
Steven Lim <stevenylim17@gmail.com>
2023-11-22 17:31:22 -0600

    replaced in-line styling with css styling for about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,78 +7,34 @@ function AboutMe() {
 
     return(
         <Fragment>
-            <div
-                style={{
-                    marginLeft: "18%",
-                    marginRight: "18%",
-                    marginTop: "6%",
-                    marginBottom: "6%",
-                }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        padding: "50px",
-                    }}
-                >
-                    <img src={placeholderImage} width={"30%"}></img>
+            <div className="Content-Area-2">
+                <div className="About-Me-Grid">
+                    <img src={placeholderImage}></img>
                     
-                    <div 
-                        style={{
-                            display:"flex",
-                            alignItems:"center",
-                        }}
-                    >
+                    <div className="About-Me-Grid-Text">
                         <p>I graduated from Texas A&M University-Commerce in 2020, majoring in Math, Physics, and Education.</p>
                     </div>
                 </div>
                 
-                <div 
-                    style={{
-                        display: "flex",
-                    }}
-                >
-                    <img src={placeholderImage} width={"30%"}></img>
-                    <div 
-                        style={{
-                            display:"flex",
-                            alignItems:"center",
-                        }}
-                    >
+                <div className="About-Me-Grid">
+                    <div className="About-Me-Grid-Text">
                         <p>After graduation, I worked in two coroporate roles, but always found my self leaning in to the more problem solving aspects of those roles. I discovered that I love learning the "how" behind all of things we experience on a daily basis in our modern society.</p>
                     </div>
+                    <img src={placeholderImage}></img>
                 </div>
                 
-                <div 
-                    style={{
-                        display: "flex",
-                    }}
-                >
-                    <img src={placeholderImage} width={"30%"}></img>
-                    <div 
-                        style={{
-                            display:"flex",
-                            alignItems:"center",
-                        }}
-                    >
+                <div className="About-Me-Grid">
+                    <img src={placeholderImage}></img>
+                    <div className="About-Me-Grid-Text">
                         <p>In the Fall of '22, I started learning. I took coursework on LinkedIn Learning, Codecademy, and Udemy over Web Development, Data Structures and Algorithms, Java, React, and HTML/CSS/JS.</p>    
                     </div>
                 </div>
 
-                <div 
-                    style={{
-                        display: "flex",
-                    }}
-                >
-                    <img src={placeholderImage} width={"30%"}></img>
-                    <div 
-                        style={{
-                            display:"flex",
-                            alignItems:"center",
-                        }}
-                    >
+                <div className="About-Me-Grid">
+                    <div className="About-Me-Grid-Text">
                         <p>After learning the basics, I began to build out a few projects that would help in my life, as well as give me experience developing. Please check out some of these projects in the following section!</p>
                     </div>
+                    <img src={placeholderImage}></img>
                 </div>
             </div>
         </Fragment>
